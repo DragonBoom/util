f73ed6fad498abd350aa727a5626d41e43d95039
DragonBoom <1312449403@qq.com>
2019-05-09 21:49:05 +0800

    Update ObjectMapperUtils.java

--- util-core/src/main/java/indi/util/ObjectMapperUtils.java
+++ util-core/src/main/java/indi/util/ObjectMapperUtils.java
@@ -5,10 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ObjectMapperUtils {
     private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    public static ObjectMapper getMapper() {
+    
+    static {
         // 反序列化时忽视bean中不存在的属性
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public static ObjectMapper getMapper() {
         return objectMapper;
     }
 }
